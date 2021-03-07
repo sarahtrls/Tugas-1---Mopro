cdc5792c2759cab244754c3184ac9fe91aa7ac49
Sarah Tri Lestari <79734754+sarahtrls@users.noreply.github.com>
2021-03-07 20:49:43 +0700

    Add files via upload

---
+++ bicycle.dart
@@ -0,0 +1,24 @@
+class bicycle {
+
+  int cadence;
+  int _speed =0;
+  int get speed => _speed;
+  int gear;
+
+  bicycle(this.cadence, this.gear);
+   
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+  void speedUp (int increment) {  
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+}
+
+void main() {
+  var bike = bicycle(2,1);
+  print(bike);
+}
